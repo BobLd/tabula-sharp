be40f33f4ae54ee3b21d0ed6fcbc56521e750843
BobLd <boris.ledeist@edhec.com>
2020-09-11 17:09:08 +0100

    Add Tabula.Json

---
+++ Tabula.Json/JSONWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Tabula.Writers;
+
+namespace Tabula.Json
+{
+    public class JSONWriter : Writer
+    {
+        public void write(StreamWriter sb, Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void write(StreamWriter sb, IReadOnlyList<Table> tables)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tabula.Json/TableSerializer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Tabula.Json
+{
+    public class TableSerializer// : JsonConverter
+    {
+        //public override bool CanConvert(Type objectType)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //[return: NullableAttribute(2)]
+        //public override object ReadJson(JsonReader reader, Type objectType, [NullableAttribute(2)] object existingValue, JsonSerializer serializer)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public override void WriteJson(JsonWriter writer, [NullableAttribute(2)] object value, JsonSerializer serializer)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
